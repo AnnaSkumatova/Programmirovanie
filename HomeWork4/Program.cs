2a27231016b42f8ee5d4b593fddc22c87ead4ecb
Anna <anut04ka2010@mail.ru>
2022-07-07 00:46:28 +0800

    Додумать

--- HomeWork4/Program.cs
+++ HomeWork4/Program.cs
@@ -1,1 +1,36 @@
-﻿Задача 1. 
+﻿/*Задача 1. Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int Summa(int a)
+{
+    int sum = 0;
+    while (a > 0)
+    {
+        sum = sum + a % 10;
+        a = a / 10;
+    }
+    return sum;
+}
+
+Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Summa of numbers {num} is {Summa(num)}");
+
+
+
+Задача 2. Напишите программу, которая задаёт массив из m элементов и выводит их на экран.*/
+
+int Array(int m)
+{
+    int n = new Random().Next(1, 101);
+    
+    int[] array = new int[n];
+    
+    return n;
+}
+
+Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Array {Array(num)}");
+
