bb4ca8983c35893ee42c807c7e3b1091613480f4
Anna <anut04ka2010@mail.ru>
2022-07-09 00:23:46 +0800

    Сделать задачу

--- HomeWork4/Program.cs
+++ HomeWork4/Program.cs
@@ -20,17 +20,19 @@ Console.WriteLine($"Summa of numbers {num} is {Summa(num)}");
 
 Задача 2. Напишите программу, которая задаёт массив из m элементов и выводит их на экран.*/
 
-int Array(int m)
+int[] Array(int m)
 {
-    int n = new Random().Next(1, 101);
-    
-    int[] array = new int[n];
-    
-    return n;
+    int size = new Random().Next(1, 101);
+    int[]array = new int[size];
+
+    for(int i = 0; i < size; i++);
+    return array;
 }
 
 Console.Write("Input number: ");
-int num = Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine($"Array {Array(num)}");
+m = array(m);
+
+Console.WriteLine($"Array {Array(m)}");
 
