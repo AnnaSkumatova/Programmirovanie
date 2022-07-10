8ad3834b8f63eb773c99b4c224e1387b16c2bc74
Anna <anut04ka2010@mail.ru>
2022-07-10 23:35:38 +0800

    Две задачи

--- HomeWork4/Program.cs
+++ HomeWork4/Program.cs
@@ -20,19 +20,33 @@ Console.WriteLine($"Summa of numbers {num} is {Summa(num)}");
 
 Задача 2. Напишите программу, которая задаёт массив из m элементов и выводит их на экран.*/
 
-int[] Array(int m)
+int[] Array(int size, int min, int max)
 {
-    int size = new Random().Next(1, 101);
-    int[]array = new int[size];
+    int[] newArray = new int[size]; 
 
-    for(int i = 0; i < size; i++);
-    return array;
+    for(int i = 0; i < size; i++)
+        newArray[i] = new Random().Next(min, max + 1);
+
+    return newArray;
 }
 
-Console.Write("Input number: ");
-int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input size of array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input min possible value of elements: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input max possible value of elements: ");
+int max = Convert.ToInt32(Console.ReadLine());
 
-m = array(m);
+int[] array = Array(size, min, max);
 
-Console.WriteLine($"Array {Array(m)}");
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    
+    Console.WriteLine();
+}
+
+ShowArray(array);
+
 
