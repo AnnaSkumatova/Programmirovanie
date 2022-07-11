2120c836bfb3a50468a55fc19a6951c811c4a901
Anna <anut04ka2010@mail.ru>
2022-07-12 00:10:54 +0800

    Задачи не закончила

--- Homework5/Program.cs
+++ Homework5/Program.cs
@@ -1,13 +1,134 @@
-﻿Задача 1. Задайте массив заполненный случайными положительными трёхзначными числами. 
-Напишите программу, которая покажет количество чётных чисел в массиве.
+﻿//Задача 1. Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+/*
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] newArray = new int[size]; 
+
+    for(int i = 0; i < size; i++)
+        newArray[i] = new Random().Next(minValue, maxValue + 1);
+
+    return newArray;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    
+    Console.WriteLine();
+}
+
+int EvenNum (int[] array)
+{
+    int count = 0;
+
+    for(int i = 0; i < array.Length; i++)
+        if(array[i] % 2 == 0) count = count + 1;
+    return count;
+}
+
+Console.Write("Input size of array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int min = 99;
+int max = 998;
+
+int[] array = CreateRandomArray(size, min, max);
+
+ShowArray(array);
+int result = EvenNum(array);
+Console.WriteLine("The number of even numbers in the array: " + result); */
 
 
 
-Задача 2. Задайте одномерный массив, заполненный случайными числами. 
-Найдите сумму элементов, стоящих на нечётных позициях.
+//Задача 2. Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] newArray = new int[size]; 
+
+    for(int i = 0; i < size; i++)
+        newArray[i] = new Random().Next(minValue, maxValue + 1);
+
+    return newArray;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    
+    Console.WriteLine();
+}
+
+int SummaElementov (int[] array)
+{
+    int sum = 0;
+
+    for(int i = 0; i < array.Length; i++)
+        if(array[i] % 2 == 1) sum = sum + array[i];
+    return sum;
+}
+
+Console.Write("Input size of array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input min possible value of elements: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input max possible value of elements: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] array = CreateRandomArray(size, min, max);
+
+ShowArray(array);
+int result = SummaElementov(array);
+Console.WriteLine("Summa elementov: " + result);
 
 
 
 
-Задача 3. Задайте массив вещественных чисел. Найдите разницу между 
-максимальным и минимальным элементов массива.
+
+
+//Задача 3. Задайте массив вещественных чисел. Найдите разницу между 
+//максимальным и минимальным элементом массива.
+
+double[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    double[] newArray = new double[size]; 
+
+    for(int i = 0; i < size; i++)
+        newArray[i] = new Random().Next(minValue, maxValue + 1);
+
+    return newArray;
+}
+
+void ShowArray(double[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    
+    Console.WriteLine();
+}
+
+int DifferenceMaxMin (double[] array)
+{
+    int min = 0;
+    int max = 0;
+
+    for(int i = 0; i < array.Length; i++)
+        if(array[i] >) sum = sum + array[i];
+    return sum;
+}
+
+Console.Write("Input size of array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input min possible value of elements: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input max possible value of elements: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] array = CreateRandomArray(size, min, max);
+
+ShowArray(array);
+int result = SummaElementov(array);
+Console.WriteLine("Summa elementov: " + result);
