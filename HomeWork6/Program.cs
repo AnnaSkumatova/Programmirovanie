d375a0f6d3503014a3a063643ff00cbb8ec1c0c3
Anna <anut04ka2010@mail.ru>
2022-07-16 01:03:27 +0800

    Выполнила

--- HomeWork6/Program.cs
+++ HomeWork6/Program.cs
@@ -48,33 +48,32 @@ Console.WriteLine("Number of numbers greater than 0: " + count);*/
 
 /*Задача 2. Напишите программу, которая найдёт точку пересечения двух прямых, 
 заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+*/
 
-
-y = 5 * x + 2
-y = 3 * x + 4
-
-x = 3    1   2
-
-y = 17   7   12
-
-y = 13   7   10 */
-
-
-Console.Write("Input 1: ");
-double b1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input 2: ");
-double b2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input 3: ");
+Console.Write("Input k1: ");
 double k1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input 4: ");
+Console.Write("Input k2: ");
 double k2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
 
-double x;
-double y;
+double x = 0;
+double y = 0;
 
-x = (b2 - b1)/(k1 - k2);
-Console.WriteLine("Res x: " + x);
+if(k1 != k2)
+{
+    x = (b2 - b1)/(k1 - k2);
+    Console.WriteLine("Axis intersection point X: " + x);
 
-y = k1 * ((b2-b1)/(k1-k2))+b1;
-Console.WriteLine("Res y: " + y);
+    y = k1 * ((b2-b1)/(k1-k2))+b1;
+    Console.WriteLine("Axis intersection point Y: " + y);
+}
+
+else
+    Console.WriteLine("No Point Intersections");
+
+
+
 
