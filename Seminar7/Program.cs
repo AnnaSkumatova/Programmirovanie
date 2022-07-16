043a9a71a871a14c1fe4ca6e9c906b4b5a0d369b
Anna <anut04ka2010@mail.ru>
2022-07-16 22:17:02 +0800

    Классная работа

--- Seminar7/Program.cs
+++ Seminar7/Program.cs
@@ -92,8 +92,45 @@ Console.WriteLine("Sum of main diagonal is " + FindDiagSum(myArray));*/
 
 
 
-Задача. Задайте двумерный массив размера m на n, каждый элемент в массиве 
+/*Задача. Задайте двумерный массив размера m на n, каждый элемент в массиве 
 находится по формуле: Aij = i+j. Выведите полученный массив на экран.
+
+int[,] Create2dArray(int rows, int columns) 
+{ 
+    int[,] newArray = new int[rows, columns]; 
+        for(int i = 0; i < rows; i++) 
+            for(int j= 0; j < columns; j++) 
+                newArray[i,j] = i + j; 
+
+    return newArray; 
+}
+
+void Show2DArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+        
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Input number of rows: "); 
+int m = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Input number of columns: "); 
+int n = Convert.ToInt32(Console.ReadLine()); 
+
+int[,] myArray = Create2dArray(m,n); 
+Show2DArray(myArray); 
+
+
+*/
+
+
+/*
+Задача. Задайте двумерный массив. Найдите элементы, у которых оба индекса 
+чётные, и замените эти элементы на их квадраты.*/
 
 int[,] CreateRandom2DArray(int rows, int columns, int minValue, int maxValue) //rows - строка, columns - столбец
 {
@@ -117,18 +154,14 @@ void Show2DArray(int[,] array)
     }
 }
 
-int FindDiagSum(int[,] array)
-{
-    int sum = 0;
+int[,] Quart (int[,] array) 
+{ 
+    for(int i = 0; i < array.GetLength(0); i++) 
+        for(int j= 1; j < array.GetLength(1); j++) 
+            if(i % 2 == 0 && j % 2 == 0) array[i,j] = array[i,j] * array[i,j]; 
 
-    if(array.GetLength(0) == array.GetLength(1))
-        for(int i = 0; i < array.GetLength(0); i++)
-            sum +=array[i,i];
-
-    return sum;
-    
-}
-
+    return array; 
+} 
 Console.Write("Input number of rows: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input number of columns: ");
@@ -138,13 +171,9 @@ int min = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input max possible value: ");
 int max = Convert.ToInt32(Console.ReadLine());
 
-int[,] myArray = CreateRandom2DArray(m, n, min, max);
+int[,] myArray = CreateRandom2DArray(m,n,min,max); 
+Show2DArray(myArray); 
+Console.WriteLine(); 
+
+myArray = Quart(myArray); 
 Show2DArray(myArray);
-
-
-
-
-
-
-Задача. Задайте двумерный массив. Найдите элементы, у которых оба индекса 
-чётные, и замените эти элементы на их квадраты.
