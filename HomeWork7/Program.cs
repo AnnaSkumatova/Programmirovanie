dfc09f3ec059e6572096922135304ab745f2cc3e
Anna <anut04ka2010@mail.ru>
2022-07-21 00:52:26 +0800

    Готово.

--- HomeWork7/Program.cs
+++ HomeWork7/Program.cs
@@ -41,15 +41,15 @@ Show2DArray(myArray); */
 
 
 /*Задача. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает 
-значение этого элемента или же указание, что такого элемента нет.
+значение этого элемента или же указание, что такого элемента нет.*/
 
-int[,] CreateRandom2DArray(int rows, int columns, int minValue, int maxValue) 
+int[,] CreateRandom2DArray(int rows, int columns) 
 {
     int[,] newArray = new int[rows, columns];
 
     for(int i = 0; i < rows; i++)
         for(int j = 0; j < columns; j++)
-            newArray[i,j] = new Random().Next(minValue, maxValue + 1);
+            newArray[i,j] = new Random().Next(0, 51);
 
     return newArray;
 } 
@@ -65,54 +65,31 @@ void Show2DArray(int[,] array)
     }
 }
 
-int MeaningElementArray(int[,] array)
+
+void MeaningElementArray(int[,] array)
 {
     Console.Write("Input number rows: ");
-    int a = Convert.ToInt32(Console.ReadLine());
+    int i = Convert.ToInt32(Console.ReadLine());
     Console.Write("Input number columns: ");
-    int b = Convert.ToInt32(Console.ReadLine()); 
-    
-    
-    if(a < array.GetLength(0) && b < array.GetLength(1))
-    {
-        for(int i = 0; i < array.GetLength(0); i++)
-            for(int j = 0; j < array.GetLength(1); j++)
-            return array[i,j];
-    }
+    int j = Convert.ToInt32(Console.ReadLine()); 
 
+                
+    if(i < array.GetLength(0) && j < array.GetLength(1))
+        Console.WriteLine(array [i,j]);
     else
         Console.WriteLine("There is no such number in the array");
-       
 }
 
 Console.Write("Input number of rows: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input number of columns: ");
 int n = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input min possible value: ");
-int min = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input max possible value: ");
-int max = Convert.ToInt32(Console.ReadLine());
 
 
-int[,] myArray = CreateRandom2DArray(m, n, min, max);
+
+int[,] myArray = CreateRandom2DArray(m, n);
 Show2DArray(myArray);
-
-Console.WriteLine("Array element value: " + MeaningElementArray(myArray));*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+MeaningElementArray(myArray);
 
 
 
@@ -122,7 +99,7 @@ Console.WriteLine("Array element value: " + MeaningElementArray(myArray));*/
 
 
 /*Задача. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
-*/
+
 int[,] CreateRandom2DArray(int rows, int columns) 
 {
     int[,] newArray = new int[rows, columns];
@@ -144,22 +121,7 @@ void Show2DArray(int[,] array)
         Console.WriteLine();
     }
 }
-double FindAverage (int[,] array)
-{
-    double average = 0;
-
-    for(int i = 0; i < array.GetLength(0); i++)
-        for(int j = 0; j < array.GetLength(1); j++)
-        {
-            int summ = 0;
-            
-            summ += array[j,i];
-            average = summ/array.GetLength(0);
-            
-        }
-    return average;    
-}
-
+ 
 
 Console.Write("Input number of rows: ");
 int m = Convert.ToInt32(Console.ReadLine());
@@ -169,4 +131,17 @@ int n = Convert.ToInt32(Console.ReadLine());
 int[,] myArray = CreateRandom2DArray(m, n);
 Show2DArray(myArray);
 
-Console.WriteLine("Average: " + FindAverage(myArray));
+for (int i = 0; i < n; i++)
+    {
+        double summ = 0;
+        for (int j = 0; j < m; j++)
+            {
+                summ = summ + myArray[j,i];
+            }
+            double average = summ / m;
+            int a = i + 1;
+        Console.WriteLine("Average for " + a +" column is " + average);    
+    } 
+
+*/
+  
