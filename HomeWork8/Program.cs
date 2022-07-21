df4bee1472231e9c19d3c1eba45541c82d6f38ce
Anna <anut04ka2010@mail.ru>
2022-07-22 00:56:42 +0800

    Еще думать и думать.

--- HomeWork8/Program.cs
+++ HomeWork8/Program.cs
@@ -1,24 +1,14 @@
-﻿Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки 
+﻿/*Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки 
 двумерного массива.
 
-Например, задан массив:
-1 4 7 2
-5 9 2 3
-8 4 2 4
 
-В итоге получается вот такой массив:
-1 2 4 7
-2 3 5 9
-2 4 4 8
-
-
-int[,] CreateRandom2DArray(int rows, int columns, int minValue, int maxValue) 
+int[,] CreateRandom2DArray(int rows, int columns) 
 {
     int[,] newArray = new int[rows, columns];
 
     for(int i = 0; i < rows; i++)
         for(int j = 0; j < columns; j++)
-            newArray[i,j] = new Random().Next(minValue, maxValue + 1);
+            newArray[i,j] = new Random().Next(10, 31);
 
     return newArray;
 } 
@@ -31,30 +21,46 @@ void Show2DArray(int[,] array)
             Console.Write(array[i, j] + " ");
         
         Console.WriteLine();
-    }
+    }   
 }
+
+int[,] Sortirovka (int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1) - 1; j++)
+            for (int z = 0; z < array.GetLength(1) - 1; z++)
+                if (array[i, z] < array[i, z + 1]) //для изменения сортировки поменять знак < на противоположный
+                    {
+                        int temp = 0;
+                        temp = array[i, z];
+                        array[i, z] = array[i, z + 1];
+                        array[i, z + 1] = temp;
+                    }
+    return array;
+}
+Console.WriteLine();
+
 
 Console.Write("Input number of rows: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input number of columns: ");
 int n = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input min possible value: ");
-int min = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input max possible value: ");
-int max = Convert.ToInt32(Console.ReadLine());
 
-int[,] myArray = CreateRandom2DArray(m, n, min, max);
+int[,] myArray = CreateRandom2DArray(m, n);
 
 Show2DArray(myArray);
 
+Console.WriteLine();
+
+Show2DArray(Sortirovka(myArray));
+*/
 
 
 
 
 
 
-
-
+/*
 Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей 
 суммой элементов.
 
@@ -65,16 +71,16 @@ Show2DArray(myArray);
 5 2 6 7
 
 Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 
-1 строка
-int[,] CreateRandom2DArray(int rows, int columns, int minValue, int maxValue) 
+1 строка*/
+
+int[,] CreateRandom2DArray(int rows, int columns) 
 {
     int[,] newArray = new int[rows, columns];
 
     for(int i = 0; i < rows; i++)
         for(int j = 0; j < columns; j++)
-            newArray[i,j] = new Random().Next(minValue, maxValue + 1);
-
-    return newArray;
+            newArray[i,j] = new Random().Next(0, 10);
+    return newArray;    
 } 
 
 void Show2DArray(int[,] array)
@@ -88,23 +94,90 @@ void Show2DArray(int[,] array)
     }
 }
 
+
+int SumLineElements(int[,] array, int i)
+{
+    int minSum = 0;
+int sumLine = SumLineElements(array, 0);
+for (int i = 1; i < array.GetLength(0); i++)
+{
+  int tempSumLine = SumLineElements(array, i);
+  if (sumLine > tempSumLine)
+  {
+    sumLine = tempSumLine;
+    minSum = i;
+  }
+
+Console.WriteLine($"\n{minSumLine+1} - строкa с наименьшей суммой ({sumLine}) элементов ");
+}
+  int sumLine = array[i,0];
+  for (int j = 1; j < array.GetLength(1); j++)
+  {
+    sumLine += array[i,j];
+  }
+  return sumLine;
+}
+
+// for (int i = 0; i < n; i++)
+//     {
+//         double summ = 0;
+//         for (int j = 0; j < m; j++)
+//             {
+//                 summ = summ + myArray[j,i];
+//             }
+//             double average = summ / m;
+//             int a = i + 1;
+//         Console.WriteLine("Average for " + a +" column is " + average);    
+//     } 
+
 Console.Write("Input number of rows: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input number of columns: ");
 int n = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input min possible value: ");
-int min = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input max possible value: ");
-int max = Convert.ToInt32(Console.ReadLine());
 
-int[,] myArray = CreateRandom2DArray(m, n, min, max);
+
+int[,] myArray = CreateRandom2DArray(m, n);
 
 Show2DArray(myArray);
 
 
 
+// for (int i = 0; i < n; i++)
+//     {
+//         int sum = 0;
+//         for (int j = 0; j < m; j++)
+//                 sum = sum + myArray[i, j];        
+            
+//             if (sum < minsum)
+//                 {
+//                     minsum = sum;
+//                     indexLine++;
+//                 }
+        
+//     }
+// Console.WriteLine("Cтрока с наименьшей суммой элементов: " + (indexLine));
+
+// int minsum = 0;
+// int indexLine = 0;
+
+// for (int i = 0; i < n; i++)
+//     {
+//         int summ = 0;
+//         for (int j = 0; j < m; j++)
+//             {
+//                 summ = summ + myArray[i,j];
+//             }
+//             if (summ < minsum)
+//                 minsum = summ;
+//                 int a = i + 1;
+            
+            
+//     Console.WriteLine("Cтрока с наименьшей суммой элементов: " + indexLine);
+//     }   
 
 
+
+/*
 Задача. Заполните спирально массив 4 на 4.
 
 Например, на выходе получается вот такой массив:
@@ -112,4 +185,4 @@ Show2DArray(myArray);
 1 2 3 4
 12 13 14 5
 11 16 15 6
-10 9 8 7
+10 9 8 7*/
