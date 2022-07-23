7bdf3d6c8457a0e1eadd084c6b29cda701940ea8
Anna <anut04ka2010@mail.ru>
2022-07-24 02:35:50 +0800

    Половинка.

---
+++ HomeWork9/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача. Задайте значение N. Напишите программу, которая найдет кол-во цифр в числе N рекурсивным методом.
+N = 4532 -> 4*/
+
+
+
+
+
+/*Задача. Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов 
+в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+
+Console.Write("Input first number: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+int SumOfDigits(int m, int n)
+{
+    if (m == n)
+        return n + SumOfDigits(m, n - 1);
+    return n;
+}
+Console.Write(SumOfDigits(m, n));
