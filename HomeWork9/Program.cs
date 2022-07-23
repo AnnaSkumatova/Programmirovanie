9548a212aefbb69a76c110f9cbcd1f03854e5f77
Anna <anut04ka2010@mail.ru>
2022-07-24 02:57:03 +0800

    Сделала.

--- HomeWork9/Program.cs
+++ HomeWork9/Program.cs
@@ -1,6 +1,18 @@
 ﻿/*Задача. Задайте значение N. Напишите программу, которая найдет кол-во цифр в числе N рекурсивным методом.
 N = 4532 -> 4*/
 
+Console.Write("Input number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int CountOfDigits(int n)
+{
+    if(n < 10)
+        return 1;
+    else return CountOfDigits(n / 10) + 1;
+}
+
+Console.WriteLine(CountOfDigits(n));
+
 
 
 
@@ -8,7 +20,7 @@ N = 4532 -> 4*/
 /*Задача. Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов 
 в промежутке от M до N.
 M = 1; N = 15 -> 120
-M = 4; N = 8. -> 30*/
+M = 4; N = 8. -> 30
 
 Console.Write("Input first number: ");
 int m = Convert.ToInt32(Console.ReadLine());
@@ -19,7 +31,8 @@ int n = Convert.ToInt32(Console.ReadLine());
 int SumOfDigits(int m, int n)
 {
     if (m == n)
-        return n + SumOfDigits(m, n - 1);
-    return n;
+        return n;
+    return n + SumOfDigits(m, n - 1);
+    
 }
-Console.Write(SumOfDigits(m, n));
+Console.Write(SumOfDigits(m, n));*/
